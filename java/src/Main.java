e1ca427ebe50b3de90cfd26062814ecd5f7b17c2
Hyeonsu Oh <hans324oh@gmail.com>
2022-01-15 22:34:30 +0900

    first commit

---
+++ java/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int arr[] = new int[26];
+        int maxUsage;
+        a = a.toUpperCase();
+        for (int i = 0; i < a.length(); i++) {
+            arr[(int)(a.indexOf(i)-'A')]++;
+        }
+        maxUsage = arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(maxUsage<arr[i])
+                maxUsage = arr[i];
+            if (i + 1 == arr.length) {
+                if(maxUsage==arr[i])
+                    System.out.printf("?");
+                else
+                    System.out.printf("%c", 'A'+);
+            }
+        }
+    }
+}
